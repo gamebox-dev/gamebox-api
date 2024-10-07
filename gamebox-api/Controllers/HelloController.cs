f503b42fcfd25fb84562cb9c03c211a08c7df358
FusTaFah <mfadel_1990@hotmail.com>
2024-10-07 17:13:10 +0100

    adds get response for hello

---
+++ gamebox-api/Controllers/HelloController.cs
@@ -0,0 +1,19 @@
+﻿using gamebox_api.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace gamebox_api.Controllers
+{
+    [Produces("application/json")]
+    [Route("[controller]")]
+    [ApiController]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet(Name = "Hello")]
+        public Hello Get()
+        {
+            return new Hello() { Status = "ok" };
+        }
+    }
+}
---
+++ gamebox-api/Models/Hello.cs
@@ -0,0 +1,7 @@
+﻿namespace gamebox_api.Models
+{
+    public class Hello
+    {
+        public string Status { get; set; }
+    }
+}
