a409b86028ffdd11619038d12e080600d4e348bd
Mustafa Fadel <mfadel_1990@hotmail.com>
2024-10-15 04:50:01 +0100

    adds documentation for gamesource interface

--- gamebox/Connectors/IGameSource.cs
+++ gamebox/Connectors/IGameSource.cs
@@ -2,8 +2,16 @@
 
 namespace GameBox.Connectors
 {
+    /// <summary>
+    /// Searches for games on external APIs
+    /// </summary>
     public interface IGameSource
     {
+        /// <summary>
+        /// Searches for a game by exact name
+        /// </summary>
+        /// <param name="q"></param>
+        /// <returns></returns>
         Task<ExternalGame>? SearchGames(string q);
     }
 }
